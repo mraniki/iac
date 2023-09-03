25cad17e63f6c9a1cc9ea7fdb653ffde5ef2a289
mraniki <8766259+mraniki@users.noreply.github.com>
2023-09-03 09:35:09 +0200

    Create variables.tf

---
+++ iac/oracle/variables.tf
@@ -0,0 +1,9 @@
+variable "COMPARTMENT_ID" {
+  type        = string
+  description = "The compartment to create the resources in"
+}
+
+variable "REGION" {
+  type        = string
+  description = "The region to provision the resources in"
+}
